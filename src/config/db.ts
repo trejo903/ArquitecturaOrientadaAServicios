bd2bcc14af88165170c9f9712fb6a42c3c888a51
Diego Trejo <124468131+trejo903@users.noreply.github.com>
2025-07-12 12:25:38 -0600

    12/07/2025

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,6 +1,9 @@
 import {Sequelize} from 'sequelize-typescript'
+import dotenv from 'dotenv';
 
-export const db = new Sequelize("postgresql://neondb_owner:npg_MmLSPGNbC1U5@ep-silent-flower-aetngxfp-pooler.c-2.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require",{
+dotenv.config();
+
+export const db = new Sequelize(process.env.DATABASE_URL!,{
     models:[__dirname+"/../models/**/*"],
     logging:false,
     dialectOptions:{
