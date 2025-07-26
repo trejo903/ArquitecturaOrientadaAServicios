89b332aebabded6269795735ddced061d9c4034e
Diego Trejo <124468131+trejo903@users.noreply.github.com>
2025-07-26 13:14:18 -0600

    26/07/2025

--- src/utils/whatsappHelper.ts
+++ src/utils/whatsappHelper.ts
@@ -4,7 +4,7 @@ import axios from 'axios'
 const WA_URL = `https://graph.facebook.com/v16.0/654446084427127/messages`
 const HEADERS = {
   'Content-Type': 'application/json',
-  Authorization: `Bearer ${process.env.WHATSAPP_TOKEN}`,
+  Authorization: `Bearer EAARt5paboZC8BPDbqIjocLuI5fEcQJI3ngJ1ZAZCRIVz8ZAEbscplO114MZB76jIfWV79pjLxw4cwNLN0y22Br4qZCLCvNj37bnZAPdcwY8lT2SphYkqzH1anHiQ5yhboAxt5aWlUX7mZCMdM0ZBcYl9WS4yeZC9QmppLnf4GFfqir7LsV9XhDZBJvcpslHKRmgF2ddZAzQbMDRUC603QSPjSkm1KLZB1Ej4EltUnPuXOVyzc`,
 }
 
 /**
