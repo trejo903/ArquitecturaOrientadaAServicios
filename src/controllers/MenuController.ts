1532353dc9d98e3603b00ce6298a60398faffe82
Diego Trejo <124468131+trejo903@users.noreply.github.com>
2025-08-02 10:27:11 -0600

    02/08/2025

--- src/controllers/MenuController.ts
+++ src/controllers/MenuController.ts
@@ -11,7 +11,7 @@ import { sendText, sendList } from '../utils/whatsappHelper'
 interface Session { paso: string; data: any }
 const sessions = new Map<string, Session>()
 
-const verifyToken = 'EAARt5paboZC8BPDbqIjocLuI5fEcQJI3ngJ1ZAZCRIVz8ZAEbscplO114MZB76jIfWV79pjLxw4cwNLN0y22Br4qZCLCvNj37bnZAPdcwY8lT2SphYkqzH1anHiQ5yhboAxt5aWlUX7mZCMdM0ZBcYl9WS4yeZC9QmppLnf4GFfqir7LsV9XhDZBJvcpslHKRmgF2ddZAzQbMDRUC603QSPjSkm1KLZB1Ej4EltUnPuXOVyzc'
+const verifyToken = 'EAARt5paboZC8BPNhbqZCHIPrZAIItzL4FEyEAXYL0x8R0aEOoD8G7i8tLs5YBrUz6O1iGM0vmw10cUuhYQu0SlAFE5xAPNsmoda8fmkHapO8yFZA4JqNUrSts0UDyBNCDd9yWZAUfJOKFAnwtfAUCSkJllgmZCqRgLS5x3jUGfnlZBZCM4gdjDRPv7VLJ88smCuLHdAGtK7ukTBl1uXzKt8P1ExvSFxCwMJ9gruw0dufCpaO3ipTxXr3zSkvl37ylAZDZD'
 
 export class MenuController {
   // GET /webhook → verificación
